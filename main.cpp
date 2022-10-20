51e72ba80a8179975ef3d395e0fec49f215981f6
Devin Hill <33045257+iamdevinhill@users.noreply.github.com>
2022-10-20 18:00:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // Devin Hill
 // Open Lab 3: Textemon Battle
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -12,19 +13,18 @@ string user_select;
 int rand_attack;
 
 // function names
-void outputHealth(string Textemon, int x);
-int loudSoundPunch(string Textemon, int x);
-int specialBeamAttack(string Textemon, int x);
-int bandaids(string Textemon, int x);
+void outputHealth(string, int);
+int loudSoundPunch(string, int);
+int specialBeamAttack(string, int);
+int bandaids(string, int);
 void runAway();
-int aiAttack(string ai_select);
-int userAttack(string );
+int aiAttack(string, int);
+int userAttack(string);
 
 
 int main() {
 
   // variables 
-   
     string ai_select = "BattleMonX";
     int ai_hp;
     int user_hp;
@@ -55,63 +55,44 @@ int main() {
   cout << "2. SPECIALBEAMATTACK" << " " << "4. RUNAWAY" << endl;
   cout << "Choice: ";
   cin >> attack_choice;
-
-   //pass to user user_select and attack_choice
    
 
    // attack decisions
-  // if (attack_choice > 0 && attack_choice < 5){
-   // if (attack_choice > 0 && attack_choice < 3){
+while (attack_choice < 1 || attack_choice > 4){
+      cout << "Please select a valid ability!" << endl;
+      cin >> attack_choice;
+    }
    
    if( attack_choice==4){
      userAttack(user_select);
-      cout <<"e5";
-     outputHealth(ai_select, ai_healthhp);
-   outputHealth(user_select, user_healthhp);
+   //   outputHealth(ai_select, ai_healthhp);
+   // outputHealth(user_select, user_healthhp);
      
-  damage =  aiAttack(ai_select);
-   user_healthhp -= damage;
-     }
-     else if(attack_choice ==1 || attack_choice ==2)
-   {  damage = userAttack(user_select);
-    ai_healthhp -= damage;
+     } else if(attack_choice ==1 || attack_choice ==2){
+     damage = userAttack(user_select);
+      ai_healthhp -= damage;
    
-   outputHealth(ai_select, ai_healthhp);
-   outputHealth(user_select, user_healthhp);
+   // outputHealth(ai_select, ai_healthhp);
+   // outputHealth(user_select, user_healthhp);
      
-  damage =  aiAttack(ai_select);
-   user_healthhp -= damage;
-  }
-   else if ( attack_choice==3)
-   {
+     } else if ( attack_choice==3){
      damage = userAttack(user_select);
     user_healthhp += damage;
    
-   outputHealth(ai_select, ai_healthhp);
-   outputHealth(user_select, user_healthhp);
-     
-    damage =  aiAttack(ai_select);
-   user_healthhp -= damage;
-     
-   }
-     
- else{cout << "Please select a valid ability!" << endl;}
-
-     // maybe fixes bandaids? ---------------------
-   //   } else if (attack_choice == 3){
-   //   damage = userAttack(user_select);
-   //  ai_healthhp += damage;
-   //  user_healthhp += damage;
    // outputHealth(ai_select, ai_healthhp);
    // outputHealth(user_select, user_healthhp);
-   // aiAttack(ai_select);
-     // --------------------------------------------
-    // } else {
-    // cout << "Please select a valid ability!" << endl;
-    //  cout << endl;
-    // cout << endl;
-    // cout << endl;
-    // }
+   }
+     rand_attack = rand() % (4 + 1);
+
+    if (rand_attack == 4){
+      aiAttack(ai_select, rand_attack);
+    } else if (rand_attack == 1 || rand_attack == 2){
+      damage = aiAttack(ai_select, rand_attack);
+      user_healthhp -= damage;
+    } else if (rand_attack == 3){
+      damage = aiAttack(ai_select, rand_attack);
+      ai_healthhp += damage;
+    }
    } 
 } 
 
@@ -156,52 +137,50 @@ void runAway(string Textemon){
     cout << endl;
     cout << endl;
     cout << Textemon << " tried to run away..." <<     endl;
-   // cout << "too bad you can't in our awesome game!" << endl;
-  // cout <<"e2";
+   cout << "too bad you can't in our awesome game!" << endl;
 }
 
-int aiAttack(string ai_select){
-   rand_attack = rand() % (4 + 1);
-  while (rand_attack <= 0 || rand_attack >=5){
-     cout << "Please select a valid ability!" << endl;
-     rand_attack = rand() % (4 + 1);
-   }  if (rand_attack == 1){
-      hp = loudSoundPunch(ai_select, hp);
+int aiAttack(string ai_select, int rand_attack){
+
+  if (rand_attack == 1){
+    hp = loudSoundPunch(ai_select, hp);
     return hp;
-    } else if (rand_attack == 2){
-     hp = specialBeamAttack(ai_select, hp);
+  }
+  else if (rand_attack == 2)
+  {
+    hp = specialBeamAttack(ai_select, hp);
     return hp;
-    } else if (rand_attack == 3){
+  }
+  else if (rand_attack == 3)
+  {
     hp = bandaids(ai_select, hp);
     return hp;
-    } else if (rand_attack == 4){
-     runAway(ai_select);
-    } else {
-     cout << "Please select a valid ability!" << endl;
-     }
-  //return to main
   }
+  else if (rand_attack == 4)
+  {
+    runAway(ai_select);
+  }
+ }
 
 int userAttack(string user_select){
-  while (attack_choice <= 0 || attack_choice >=5){
-     cout << "Please select a valid ability!" << endl;
-     cin >> attack_choice;
-   } if (attack_choice == 1){
-     hp = loudSoundPunch(user_select, hp);
+  while (attack_choice <= 0 || attack_choice >= 5){
+    cout << "Please select a valid ability!" << endl;
+    cin >> attack_choice;
+  }
+  if (attack_choice == 1){
+    hp = loudSoundPunch(user_select, hp);
     return hp;
-    } else if (attack_choice == 2){
-     hp = specialBeamAttack(user_select, hp);
+  }
+  else if (attack_choice == 2){
+    hp = specialBeamAttack(user_select, hp);
     return hp;
-    } else if (attack_choice == 3){
-     hp =bandaids(user_select, hp);
+  }
+  else if (attack_choice == 3){
+    hp = bandaids(user_select, hp);
     return hp;
-    } else if (attack_choice == 4){
-    cout <<"e1";
-     runAway(user_select);
-     cout <<"e3";
-    } else {
-     cout << "Please select a valid ability!" << endl;
-     }
-  //aiattack
+  }
+  else if (attack_choice == 4){
+    runAway(user_select);
+  }
 }
 
